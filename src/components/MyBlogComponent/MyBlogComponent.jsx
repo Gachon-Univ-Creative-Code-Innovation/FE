49168a2178a3a75413e4066780ad59cf8682c3f3
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-04-18 00:24:43 +0900

    fix: 로그아웃 버튼 기능 추가 refactor: 컴포넌트 리네이밍
    JIRA: GUC-91
    resolves: #20

--- src/components/MyBlogComponent/MyBlogComponent.jsx
+++ src/components/MyBlogComponent/MyBlogComponent.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 import React from "react";
-import "./PropertyFrameWrapper.css";
+import "./MyBlogComponent.css";
 
-export const PropertyFrameWrapper = ({ property1 }) => {
+export const MyBlogComponent = ({ property1 }) => {
   return (
     <div className={`property-frame-wrapper ${property1}`}>
       <div className="text-wrapper-5">MY BLOG</div>
@@ -10,8 +10,8 @@ export const PropertyFrameWrapper = ({ property1 }) => {
   );
 };
 
-PropertyFrameWrapper.propTypes = {
+MyBlogComponent.propTypes = {
   property1: PropTypes.oneOf(["frame-117", "frame-118"]),
 };
 
-export default PropertyFrameWrapper;
+export default MyBlogComponent;
