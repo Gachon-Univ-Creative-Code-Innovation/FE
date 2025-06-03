3123bf3756c22747c47b785777a535dc52076ee4
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-06-04 01:43:42 +0900

    feat: 로그인 필요 팝업 내 로그인 버튼 추가 fix: 로그인 필요 팝업 css 수정

--- src/components/LoginRequiredPopup/LoginRequiredPopup.jsx
+++ src/components/LoginRequiredPopup/LoginRequiredPopup.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 import "./LoginRequiredPopup.css";
 import LoginComponent from "../../components/LoginComponent/LoginComponent";
 
 const MDiv = motion.div;
 
 const LoginRequiredPopup = ({ onClose }) => {
+  const navigate = useNavigate();
+
   return (
     <MDiv
       className="login-required-popup"
@@ -21,19 +23,27 @@ const LoginRequiredPopup = ({ onClose }) => {
         exit={{ scale: 0.9, opacity: 0 }}
         transition={{ duration: 0.25 }}
       >
-        <div className="login-required-popup__title">Login Required</div>
+        <div className="login-required-popup__title">로그인이 필요합니다</div>
 
         <div className="login-required-popup__description-wrapper">
           <p className="login-required-popup__description">
-            이 기능은 로그인 후 이용 가능합니다.
+            이 기능을 사용하려면 로그인이 필요합니다.
             <br />
-            로그인으로 나만의 여정을 지금 바로 이어가 보세요.
+            <br />
+            로그인 후 다시 시도해 주세요.
           </p>
         </div>
 
         <div className="login-required-popup__button-group">
           <div className="login-required-popup__button" onClick={onClose}>
-            <LoginComponent className="component-1" property1="default" />
+            <div className="login-required-popup__button-text">확인</div>
+          </div>
+
+          <div
+            className="login-required-popup__button"
+            onClick={() => navigate("/login")}
+          >
+            <div className="login-required-popup__button-text">로그인</div>
           </div>
         </div>
       </MDiv>
