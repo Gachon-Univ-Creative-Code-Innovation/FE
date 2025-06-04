4cc291467df895fbc98dae7e9b57bd6e7561f6a2
Kim-songhui <rlathdgmlkk@naver.com>
2025-06-05 01:10:49 +0900

    fix:메인페이지의 포트폴리오 광고 버튼 ui 수정

--- src/components/MakePortfolioCard/MakePortfolioCard.jsx
+++ src/components/MakePortfolioCard/MakePortfolioCard.jsx
@@ -1,22 +1,35 @@
+// src/components/MakePortfolio.jsx
 import PropTypes from "prop-types";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./MakePortfolioCard.css";
 
-export const MakePortfolio = ({ className, onClick }) => {
+export const MakePortfolio = ({ className }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/portfolio");
+  };
+
   return (
     <div
       className={`property-hover-wrapper ${className}`}
-      onClick={onClick}
+      onClick={handleClick}
       style={{ cursor: "pointer" }}
     >
-      <img className="img-front" src="/img/portfoilo_new.png" alt="front" />
+      <img className="img-front" src="/img/portfolio_new.png" alt="front" />
+
+      {/* 별 요소 (중심 회전) */}
+      <div className="sparkle-extra orbit1"></div>
+      <div className="sparkle-extra orbit2"></div>
+      <div className="sparkle-extra orbit3"></div>
+      <div className="sparkle-extra orbit4"></div>
     </div>
   );
 };
 
 MakePortfolio.propTypes = {
   className: PropTypes.string,
-  onClick: PropTypes.func,
 };
 
 export default MakePortfolio;
