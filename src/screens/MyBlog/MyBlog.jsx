a514bf7e0ddd99162e1a3305945ce73dd3a550e4
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-06-06 00:55:04 +0900

    fix: fix

--- src/screens/MyBlog/MyBlog.jsx
+++ src/screens/MyBlog/MyBlog.jsx
@@ -1,5 +1,3 @@
-// src/screens/MyBlog/MyBlog.jsx
-
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import GoGitHub from "../../components/GoGitHub/GoGitHub";
