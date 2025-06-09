04e17847bcec8c2b4da6163efcc1923605c54bdf
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-06-09 11:41:21 +0900

    fix: 마이블로그 css placeholder 수정 fix: blog css placeholder feat: 유저블로그 유저의  팔로잉 팔로워 api 연결
    JIRA: GUC-272
    resolves: #194

--- src/screens/MyBlog/MyBlog.jsx
+++ src/screens/MyBlog/MyBlog.jsx
@@ -176,7 +176,6 @@ export const MyBlog = () => {
               <div
                 className="myblog-profile-image"
                 style={{
-                  backgroundColor: "#d9d9d9",
                   backgroundImage: profileUrl ? `url(${profileUrl})` : "none",
                 }}
               />
@@ -246,7 +245,6 @@ export const MyBlog = () => {
                       <div
                         className="myblog-post-image"
                         style={{
-                          backgroundColor: "#a3b3bf",
                           backgroundImage: post.thumbnail
                             ? `url(${post.thumbnail})`
                             : "url('/img/blog_basic_photo.png')",
